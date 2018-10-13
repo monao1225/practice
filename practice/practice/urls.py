20c3f76c25bd310f452f46d93fe403ea6d605703
monao <nohr@outlook.jp>
2018-10-13 18:58:43 +0900

    更新

--- practice/practice/urls.py
+++ practice/practice/urls.py
@@ -6,8 +6,7 @@ from datetime import datetime
 from django.conf.urls import url
 import django.contrib.auth.views
 
-import app.forms
-import app.views
+import HelloDjango.views
 
 # Uncomment the next lines to enable the admin:
 # from django.conf.urls import include
@@ -15,32 +14,6 @@ import app.views
 # admin.autodiscover()
 
 urlpatterns = [
-    # Examples:
-    url(r'^$', app.views.home, name='home'),
-    url(r'^contact$', app.views.contact, name='contact'),
-    url(r'^about', app.views.about, name='about'),
-    url(r'^login/$',
-        django.contrib.auth.views.login,
-        {
-            'template_name': 'app/login.html',
-            'authentication_form': app.forms.BootstrapAuthenticationForm,
-            'extra_context':
-            {
-                'title': 'Log in',
-                'year': datetime.now().year,
-            }
-        },
-        name='login'),
-    url(r'^logout$',
-        django.contrib.auth.views.logout,
-        {
-            'next_page': '/',
-        },
-        name='logout'),
-
-    # Uncomment the admin/doc line below to enable admin documentation:
-    # url(r'^admin/doc/', include('django.contrib.admindocs.urls')),
-
-    # Uncomment the next line to enable the admin:
-    # url(r'^admin/', include(admin.site.urls)),
+   url(r'^$', HelloDjango.views.index, name='index'),
+   url(r'^home$', HelloDjango.views.index, name='home'),
 ]
