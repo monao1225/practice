66431efe287efc1cf5ab3c200677eafc98a7392f
monao <nohr@outlook.jp>
2018-11-15 19:08:11 +0900

    kousin

--- practice/HelloDjango/views.py
+++ practice/HelloDjango/views.py
@@ -1,19 +1,21 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 from datetime import datetime
+from HelloDjango.models import post
 
 def index(request):
     now = datetime.now()
+    variables = {
+        'title' : "Hello Django",
+        'message' : "Hello Django!",
+        'content' : " on " + now.strftime("%A, %d %B, %Y at %X"),
+    }
 
     return render(
         request,
         "index.html",  # Relative path from the 'templates' folder to the template file
         # "index.html", # Use this code for VS 2017 15.7 and earlier
-        {
-            'title' : "Hello Django",
-            'message' : "Hello Django!",
-            'content' : " on " + now.strftime("%A, %d %B, %Y at %X")
-        }
+        variables
     )
 def about(request):
     return render(
